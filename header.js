82346ade0abe4415a07644d3f286f886781b0207
Bub McSquirts <115965290+BubMcSquirts@users.noreply.github.com>
2025-01-14 21:04:46 -0800

    Add files via upload

--- header.js
+++ header.js
@@ -22,22 +22,6 @@ class Header extends HTMLElement {
         <a href="cubeTimer.html">Cube Timer</a>
       </div>
       <a class="headerBlockRightAbout" href="about.html">About</a>
-      <div class="headerBlockRight">
-        <img src="images/Icons/Menu_Logo_Icon.png" />
-        <div class="menuDropdownMaster">
-          <a href="index.html">Home</a>
-          <a href="music.html">Music</a>
-          <a href="3D.html">3D</a>
-          <a href="stopmotion.html">Stop Motion</a>
-          <a href="2D.html">2D</a>
-          <a href="coding.html">Coding</a>
-          <a href="film.html">Film</a>
-          <a href="photos.html">Photos</a>
-          <a href="cubeTimer.html">Cube Timer</a>
-          <a target ="_blank" href="Quiz_Website/indexQuiz.html">Quizzes</a>
-          <a href="about.html">About</a>
-        </div>
-      </div>
     </div>
         `;
   }
